8308efeae2cd2d28569a020c146bae25b5f2ccfd
dkanderson <dkanderson.designs@gmail.com>
2014-10-12 13:42:14 -0400

    rename collections to remove redundaancy

---
+++ assets/js/collections/radio.js
@@ -0,0 +1,22 @@
+define([
+    'backbone',
+    'models/radio'
+], function (Backbone, Model) {
+
+    'use strict';
+
+    var NewCollection = Backbone.Collection.extend({
+        // Reference to this collection's model.
+        model: Model,
+
+        // url: 'http://jis.gov.jm/?json=get_posts&post_type=radio_programs',
+        url: '../data/radio.json',
+
+        parse: function (resp) {
+            return resp.posts;
+        }
+
+    });
+
+    return new NewCollection();
+});
