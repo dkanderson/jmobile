3b6d4d8c52a4398d22d10ce0cc09c38ebab4b926
dkanderson <dkanderson.designs@gmail.com>
2014-10-16 04:04:45 -0400

    add basic functionality for newsletter view

---
+++ assets/js/views/newsletter.js
@@ -0,0 +1,22 @@
+define(['backbone'], function (Backbone){
+
+	'use strict';
+
+	var Newsletter = Backbone.View.extend({
+
+		className: 'newsletter',
+
+		template: $('#nlTpl').html(),
+
+		initialize: function () {
+			this.render();
+		},
+
+		render: function () {
+			this.$el.html(this.template);
+			return this;
+		}
+	});
+
+	return Newsletter;
+});
