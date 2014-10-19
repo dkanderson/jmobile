154246f059c9a891932494abccdc29f358b84d15
dkanderson <dkanderson.designs@gmail.com>
2014-10-19 07:55:09 -0400

    add photo list view

---
+++ assets/js/views/photoList.js
@@ -0,0 +1,36 @@
+define(['backbone', 'views/photo'], function (Backbone, PhotoView) {
+
+    'use strict';
+
+    var PhotoList = Backbone.View.extend({
+
+        className: 'photo-gallery',
+
+        initialize: function () {
+            this.renderAll();
+            App.moveUp('gallery-item');
+
+        },
+
+        render: function (model) {
+            var photo = new PhotoView({
+                model: model
+            });
+
+            this.$el.append(photo.render().el);
+            return this;
+        },
+
+        renderAll: function () {
+            this.collection.each(this.render, this);
+            setTimeout(function () {
+                App.photoswipe('.photo-gallery');
+            });
+
+            return this;
+        }
+
+    });
+
+    return PhotoList;
+});
