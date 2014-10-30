ded08a7ed8d6873a5376a4f943d496368c2cf2a7
dkanderson <dkanderson.designs@gmail.com>
2014-10-30 13:06:03 -0400

    use live feed

--- assets/js/collections/photo.js
+++ assets/js/collections/photo.js
@@ -9,8 +9,8 @@ define([
         // Reference to this collection's model.
         model: Model,
 
-        // url: 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=106946193@N02&format=json&nojsoncallback=1&api_key=cf3f96e93b49ff3332e89e7817b3885e',
-        url: './data/photos.json',
+        url: 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=106946193@N02&format=json&nojsoncallback=1&api_key=cf3f96e93b49ff3332e89e7817b3885e',
+        // url: './data/photos.json',
 
         parse: function (resp) {
             return resp.photos.photo;
