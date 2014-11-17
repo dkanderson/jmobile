ae872dc0632de83b9211b98230916c6325bb145e
dkanderson <dkanderson.designs@gmail.com>
2014-11-16 19:33:17 -0500

    change url and parser

--- assets/js/collections/photo.js
+++ assets/js/collections/photo.js
@@ -9,11 +9,11 @@ define([
         // Reference to this collection's model.
         model: Model,
 
-        url: 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=106946193@N02&format=json&nojsoncallback=1&api_key=cf3f96e93b49ff3332e89e7817b3885e',
+        url: 'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&user_id=106946193@N02&format=json&nojsoncallback=1&api_key=cf3f96e93b49ff3332e89e7817b3885e&page=1&per_page=20',
         // url: './data/photos.json',
 
         parse: function (resp) {
-            return resp.photos.photo;
+            return resp.photosets.photoset;
         }
 
     });
